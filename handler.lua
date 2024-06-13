5605acb7e30ab4c5cf155937079f9fccb3046152
Jack Tysoe <jack@tys.one>
2024-06-13 01:26:43 +0100

    sample

---
+++ handler.lua
@@ -0,0 +1,15 @@
+local plugin = {
+  PRIORITY = 1000,
+  VERSION = "0.1",
+}
+
+function plugin:init_worker()
+
+  kong.log.debug("saying hi from the 'init_worker' handler")
+end
+
+function plugin:access(plugin_conf)
+  kong.log.warn("--------> I AM HERE <---------")
+end
+
+return plugin
